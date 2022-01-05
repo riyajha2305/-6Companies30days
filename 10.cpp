f90e2400d7af31c9058a9e0dadf7e156ebfaa033
RiyaJha <78296094+riyajha2305@users.noreply.github.com>
2022-01-05 19:35:10 +0530

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -1,1 +1,21 @@
 // Find max 10 numbers in a list having 10M entries
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    set<int> s;
+    while(n--){
+        int a;
+        cin>>a;
+        s.insert(a);
+        if(s.size()>100){
+            s.erase(s.begin());
+        }
+    }
+    for(auto x:s){
+        cout<<x<<" ";
+    }
+}
