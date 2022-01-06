a933c928ef2974430e38fa657e4831639ca8bc43
RiyaJha <78296094+riyajha2305@users.noreply.github.com>
2022-01-07 01:51:58 +0530

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -1,1 +1,66 @@
 // Array Pair Sum Divisibility Problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool pairsPossible(int arr[], int n, int k)
+{
+    if (n & 1)
+        return false;
+
+    // Create a frequency array to count occurrences
+    // of all remainders when divided by k.
+    unordered_map<int, int> freq;
+
+    // Count occurrences of all remainders
+    for (int i = 0; i < n; i++)
+        freq[((arr[i] % k) + k) % k]++;
+
+    // Traverse input array and use freq[] to decide
+    // if given array can be divided in pairs
+    for (int i = 0; i < n; i++) {
+        // Remainder of current element
+        int rem = ((arr[i] % k) + k) % k;
+
+        //case 1
+        // If remainder with current element divides
+        // k into two halves.
+        if (2 * rem == k) {
+            // Then there must be even occurrences of
+            // such remainder
+            if (freq[rem] % 2 != 0)
+                return false;
+        }
+    
+        //case 2
+        // If remainder is 0, then there must be even 
+        // number of elements with 0 remainder
+        else if (rem == 0) {
+            if (freq[rem]%2==1)
+                return false;
+        }
+
+        //case 3
+        // Else number of occurrences of remainder
+        // must be equal to number of occurrences of
+        // k - remainder
+        else if (freq[rem] != freq[k - rem])
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int n,k;;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cin>>k;
+
+    pairsPossible(arr, n, k) ? cout << "True" : cout << "False";
+    return 0;
+}
