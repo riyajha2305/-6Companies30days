150362242053d62d7bd4679d7169cdb9f7aad271
RiyaJha <78296094+riyajha2305@users.noreply.github.com>
2022-01-07 19:10:45 +0530

    Update 17.cpp

--- 17.cpp
+++ 17.cpp
@@ -1,37 +1,34 @@
-// IPL 2021 - Match Day 2
-
-vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
-       // your code here
-       deque<int>dq(k);
-       vector<int>ans;
-       
-       int i;
-       
-       for(i=0;i<k;++i){
-           while(!dq.empty() && arr[i]>=arr[dq.back()]){
-               dq.pop_back();
-           }
-           dq.push_back(i);
-           
-       }
-       
-       for(;i<n;++i){
-           ans.push_back(arr[dq.front()]);
-           
-           //shift to new window
-           while(!dq.empty() && dq.front()<=i-k){
-               dq.pop_front();
-           }
-           
-           while(!dq.empty() && arr[i]>=arr[dq.back()]){
-               dq.pop_back();
-           }
-           
-           dq.push_back(i);
-           
-       }
-       
-       ans.push_back(arr[dq.front()]);
-       
-       return ans;
-   }
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        int count,j,mx=0,n=arr.size();
+        bool flag;
+        for(int i=1;i<n;i++){
+            j=i;
+            count=1;
+            flag=false;
+            
+            //increasing sequence
+            while(j<n && arr[j]>arr[j-1]){
+                count++;
+                j++;
+            }
+            
+            //decreasing sequence
+            while(i!=j && j<n && arr[j]<arr[j-1]){
+                count++;
+                j++;
+                flag=true;
+            }
+            
+            //length is max or not
+            if(i!=j && flag && count>=3){
+                mx=max(mx,count);
+                j--;
+            }
+            
+            i=j;
+        }
+        return mx;
+    }
+};
